d93e576e153fdaa7db0898db0f57cd8e22b6f0f5
Galaxy <satyakiran56@gmail.com>
2022-05-25 11:02:59 +0530

    added login and signin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Header from './Components/Header';
-import Banner from './Components/Banner';
-import Feed from './Components/Feed';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Components/Header";
+import Banner from "./Components/Banner";
+import Feed from "./Components/Feed";
+import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
+import { useState } from "react";
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [isSignin, setIsSignin] = useState(false);
   return (
     <div>
-      <Header />
+    {!isLogin && !isSignin &&
+      <div> 
+      <Header signin={setIsSignin}/>
       <Banner />
       <Feed />
+      </div>
+    }
+      {isSignin && <SignUp signin={setIsSignin} login={setIsLogin} />}
+      {isLogin && <Login signin={setIsSignin} login={setIsLogin} />}
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from './Header.module.css';
-const Header = () => {
+const Header = (props) => {
+  const signInHandler = () => {
+    props.signin(prevState => !prevState);
+  }
   return (
     <div className="container d-flex" style={{marginTop: 1 + "%"}}>
       <h3>ATG.WORLD</h3>
       <div className={`${styles.margin} ${styles.search}`}> 
         <input type="text" placeholder="Search for your favorite groups in ATG"></input>
       </div>
-      <div>Create account <a href="/#">It's free</a></div>
+      <div>Create account <a href="/#" onClick={signInHandler}>It's free</a></div>
     </div>
   );
 };
